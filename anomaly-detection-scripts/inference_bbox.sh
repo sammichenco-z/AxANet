3399bc6c0de13764495dd9e4a4bbb4f7f45770e2
sammichenco-z <114123483+sammichenco-z@users.noreply.github.com>
2024-03-26 19:26:37 +0800

    Delete anomaly-detection-scripts directory

--- anomaly-detection-scripts/inference_bbox.sh
+++
@@ -1,8 +0,0 @@
-# After launching the docker container 
-EVAL_DIR='output/test_detection/checkpoint-100-18500/'
-CHECKPOINT='output/test_detection/checkpoint-100-18500/model.bin'
-CUDA_VISIBLE_DEVICES=6 python -m pdb src/tasks/run_bbox_inference.py \
-       --resume_checkpoint $CHECKPOINT  \
-       --eval_model_dir $EVAL_DIR \
-       --do_lower_case \
-       --do_test 
--- anomaly-detection-scripts/visualize_attention_grad_cam.sh
+++
@@ -1,8 +0,0 @@
-# After launching the docker container 
-EVAL_DIR='output/transformer_baseline_pred_class_1layer/checkpoint-200-18400/'
-CHECKPOINT='output/transformer_baseline_pred_class_1layer/checkpoint-200-18400/model.bin'
-CUDA_VISIBLE_DEVICES=1 python -m pdb src/tasks/visualize_attention_grad_cam.py \
-       --resume_checkpoint $CHECKPOINT  \
-       --eval_model_dir $EVAL_DIR \
-       --do_lower_case \
-       --do_test 
